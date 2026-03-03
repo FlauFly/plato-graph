93cd57b446d40b50af78b7dae77129956f10c37f
FlauFly <tirhhe@gmail.com>
2026-03-03 12:10:38 +0100

    initial interaction on click

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import ForceGraph3D from "3d-force-graph";
 
 import { CSS2DRenderer, CSS2DObject } from 'https://esm.sh/three/examples/jsm/renderers/CSS2DRenderer.js';
 
+const infoCard = document.getElementById('info-card');
+
 const Graph = new ForceGraph3D(document.getElementById('3d-graph'), {
   extraRenderers: [new CSS2DRenderer()]
 })
@@ -13,6 +15,10 @@ const Graph = new ForceGraph3D(document.getElementById('3d-graph'), {
     nodeEl.style.color = node.color;
     nodeEl.className = 'node-label';
     return new CSS2DObject(nodeEl);
+  })
+  .onNodeClick(node => {
+    infoCard.innerHTML = `${node.id} <br>
+    <a href="${node.address}">Link</a>`;
   })
   .nodeThreeObjectExtend(true)
 ;
