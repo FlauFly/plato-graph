cb6d96b25cc887814675cb07dc14afad0f12f5b4
FlauFly <tirhhe@gmail.com>
2026-03-03 11:19:47 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import ForceGraph3D from "3d-force-graph";
+
+import { CSS2DRenderer, CSS2DObject } from 'https://esm.sh/three/examples/jsm/renderers/CSS2DRenderer.js';
+
+const Graph = new ForceGraph3D(document.getElementById('3d-graph'), {
+  extraRenderers: [new CSS2DRenderer()]
+})
+  .jsonUrl('../datasets/articles.json')
+  .nodeAutoColorBy('group')
+  .nodeThreeObject(node => {
+    const nodeEl = document.createElement('div');
+    nodeEl.textContent = node.id;
+    nodeEl.style.color = node.color;
+    nodeEl.className = 'node-label';
+    return new CSS2DObject(nodeEl);
+  })
+  .nodeThreeObjectExtend(true)
+;
+
+
