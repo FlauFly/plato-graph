a2421599b29a4bf17cd64dbdc1da88949e547e15
FlauFly <tirhhe@gmail.com>
2026-03-09 09:10:56 +0100

    aesthetic changes

--- src/main.js
+++ src/main.js
@@ -1,26 +1,29 @@
 import ForceGraph3D from "3d-force-graph";
 
-import { CSS2DRenderer, CSS2DObject } from 'https://esm.sh/three/examples/jsm/renderers/CSS2DRenderer.js';
+import { CSS2DRenderer, CSS2DObject } from "three/examples/jsm/Addons.js";
+
+import SpriteText from "three-spritetext";
 
 const infoCard = document.getElementById('info-card');
 
-const Graph = new ForceGraph3D(document.getElementById('3d-graph'), {
-  extraRenderers: [new CSS2DRenderer()]
-})
+const Graph = new ForceGraph3D(document.getElementById('3d-graph'))
   .jsonUrl('../datasets/articles.json')
-  .nodeAutoColorBy('group')
+  .nodeColor(node => '#ee4fc5')
   .nodeThreeObject(node => {
-    const nodeEl = document.createElement('div');
-    nodeEl.textContent = node.id;
-    nodeEl.style.color = node.color;
-    nodeEl.className = 'node-label';
-    return new CSS2DObject(nodeEl);
+    const sprite = new SpriteText(node.id);
+    sprite.material.depthWrite = false;
+    sprite.color = '#fff';
+    sprite.textHeight = 8;
+    sprite.center.y = -0.6;
+    return sprite;
   })
   .onNodeClick(node => {
-    infoCard.innerHTML = `${node.id} <br>
+    infoCard.innerHTML = `
+    <div>${node.id}</div>
     <a href="${node.address}">Link</a>`;
   })
-  .nodeThreeObjectExtend(true)
-;
+  .nodeThreeObjectExtend(true);
+
+  Graph.d3Force('charge').strength(-120);
 
 
